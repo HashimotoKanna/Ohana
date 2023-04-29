d39d379b2d38f72cc3e48546b54f0b8c20f8833e
HashimotoKanna <it0_s0u@icloud.com>
2023-04-30 03:21:28 +0900

    ganbatta

--- MinerGame-master/assets/cogs/database/button.py
+++ MinerGame-master/assets/cogs/database/button.py
@@ -1,6 +1,7 @@
 import discord
 from . import database
 from aiosqlite import connect
+
 IMG_PATH = "C:/Users/it0_s/PycharmProjects/MinerGame-master/MinerGame-master/assets/img"
 DB_PATH = "C:/Users/it0_s/PycharmProjects/MinerGame-master/MinerGame-master/assets/db/mine.db"
 BG_PATH = f'{IMG_PATH}/background.png'
@@ -13,6 +14,21 @@ class Confirm(discord.ui.View):
         super().__init__()
         self.value = None
 
+    async def embed_response(self, interaction, x, y):
+        user_id = interaction.user.id
+        Mine = database.Mine(interaction=interaction)
+        fname = "playing_front.png"
+        async with connect(DB_PATH) as conn:
+            async with conn.cursor() as cur:
+                depth, mine_text, layer = await Mine.player_mine(user_id, x, y, conn, cur)
+                depth = (layer - 1) * 20 + depth[1]
+                text = f"{mine_text}\n\n現在深度{depth}"
+
+                embed = discord.Embed(description=text)
+                file = discord.File(fp=IMG_PATH + "/" + fname, spoiler=False)
+                embed.set_image(url=f"attachment://{fname}")
+                await interaction.response.send_message(file=file, embed=embed, view=Confirm())
+
     @discord.ui.button(label=u"\u200b", style=discord.ButtonStyle.grey, row=1)
     async def none0(self, interaction: discord.Interaction, button: discord.ui.Button):
         await interaction.response.defer()
@@ -20,18 +36,7 @@ class Confirm(discord.ui.View):
 
     @discord.ui.button(emoji='⬆️', style=discord.ButtonStyle.green, row=1)
     async def arrow_up(self, interaction: discord.Interaction, button: discord.ui.Button):
-        user_id = interaction.user.id
-        Mine = database.Mine(interaction=interaction)
-        fname = "playing_front.png"
-        async with connect(DB_PATH) as conn:
-            async with conn.cursor() as cur:
-                depth, mine_text, layer = await Mine.player_mine(user_id, 0, -1, conn, cur)
-                depth = (layer - 1) * 20 + depth[1]
-                text = f"{mine_text}\n\n現在深度{depth}"
-
-                embed = discord.Embed(description=text)
-                file = discord.File(fp=IMG_PATH + "/" + fname, spoiler=False)
-                embed.set_image(url=f"attachment://{fname}")
+        await self.embed_response(interaction, 0, -1)
 
     @discord.ui.button(label=u"\u200b", style=discord.ButtonStyle.grey, row=1)
     async def none1(self, interaction: discord.Interaction, button: discord.ui.Button):
@@ -40,16 +45,7 @@ class Confirm(discord.ui.View):
 
     @discord.ui.button(emoji='⬅️', style=discord.ButtonStyle.green, row=2)
     async def arrow_left(self, interaction: discord.Interaction, button: discord.ui.Button):
-        user_id = interaction.user.id
-        async with connect(DB_PATH) as conn:
-            async with conn.cursor() as cur:
-                player_depth = await database.player_mine(user_id, -1, 0, conn, cur)  # プレイヤーの深度を取得
-                await database.mine_(user_id, cur)
-                fname = "playing_front.gif"
-                embed = discord.Embed(description=f"一マス掘りました！\n\n現在深度{player_depth}")
-                file = discord.File(fp=IMG_PATH + "/" + fname, spoiler=False)  # ローカル画像からFileオブジェクトを作成
-                embed.set_image(url=f"attachment://{fname}")
-                await interaction.response.send_message(file=file, embed=embed)
+        await self.embed_response(interaction, -1, 0)
 
     @discord.ui.button(label=u"\u200b", style=discord.ButtonStyle.grey, row=2)
     async def none2(self, interaction: discord.Interaction, button: discord.ui.Button):
@@ -58,16 +54,7 @@ class Confirm(discord.ui.View):
 
     @discord.ui.button(emoji='➡️', style=discord.ButtonStyle.green, row=2)
     async def arrow_right(self, interaction: discord.Interaction, button: discord.ui.Button):
-        user_id = interaction.user.id
-        async with connect(DB_PATH) as conn:
-            async with conn.cursor() as cur:
-                player_depth = await database.player_mine(user_id, 1, 0, conn, cur)  # プレイヤーの深度を取得
-                await database.mine_(user_id, cur)
-                fname = "playing_front.gif"
-                embed = discord.Embed(description=f"一マス掘りました！\n\n現在深度{player_depth}")
-                file = discord.File(fp=IMG_PATH + "/" + fname, spoiler=False)  # ローカル画像からFileオブジェクトを作成
-                embed.set_image(url=f"attachment://{fname}")
-                await interaction.response.send_message(file=file, embed=embed)
+        await self.embed_response(interaction, 1, 0)
 
     @discord.ui.button(label=u"\u200b", style=discord.ButtonStyle.grey, row=3)
     async def none3(self, interaction: discord.Interaction, button: discord.ui.Button):
@@ -76,16 +63,7 @@ class Confirm(discord.ui.View):
 
     @discord.ui.button(emoji='⬇️', style=discord.ButtonStyle.green, row=3)
     async def arrow_down(self, interaction: discord.Interaction, button: discord.ui.Button):
-        user_id = interaction.user.id
-        async with connect(DB_PATH) as conn:
-            async with conn.cursor() as cur:
-                player_depth = await database.player_mine(user_id, 0, -1, conn, cur)  # プレイヤーの深度を取得
-                await database.mine_(user_id, cur)
-                fname = "playing_front.gif"
-                embed = discord.Embed(description=f"一マス掘りました！\n\n現在深度{player_depth}")
-                file = discord.File(fp=IMG_PATH + "/" + fname, spoiler=False)  # ローカル画像からFileオブジェクトを作成
-                embed.set_image(url=f"attachment://{fname}")
-                await interaction.response.send_message(file=file, embed=embed)
+        await self.embed_response(interaction, 0, 1)
 
     @discord.ui.button(label=u"\u200b", style=discord.ButtonStyle.grey, row=3)
     async def none4(self, interaction: discord.Interaction, button: discord.ui.Button):
